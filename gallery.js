4ed86b5985981eb4fdbd32a73fe221b25d28e2b6
JOOSA <joonas.salonpaa@vaisala.com>
2022-06-07 19:48:36 +0300

    Add possibility to add uncredited photos & remove credits from newest photos

--- gallery.js
+++ gallery.js
@@ -1,4 +1,9 @@
 function changePic(pic, credits) {
     document.getElementById('bio-pic').src = pic
+    if (credits) {
+      document.getElementById('photo-by-container').style.display = ''
+    } else {
+      document.getElementById('photo-by-container').style.display = 'none'
+    }
     document.getElementById('photo-by').innerText = credits
 }
