0a670849acb3aa9b9c99f8c5c02b823812682197
Joonas Salonpää <joonas.salonpaa@vaisala.com>
2026-03-25 11:04:26 +0200

    Updates as requested
    Also, modify the photo credits so that the whole container is not
    hidden but it only contains one nonbreaking space if there are no
    credits -> page is not jumping when switching between credited and
    non-credited photos.

--- gallery.js
+++ gallery.js
@@ -1,9 +1,10 @@
 function changePic(pic, credits) {
     document.getElementById('bio-pic').src = pic
     if (credits) {
-      document.getElementById('photo-by-container').style.display = ''
+      document.getElementById('photo-by-pre').style.display = ''
+      document.getElementById('photo-by').innerText = credits
     } else {
-      document.getElementById('photo-by-container').style.display = 'none'
+      document.getElementById('photo-by-pre').style.display = 'none'
+      document.getElementById('photo-by').innerHTML = '&nbsp;'
     }
-    document.getElementById('photo-by').innerText = credits
 }
